7ccb9ea77afc305ce32a95eba85133f7dec3a5ff
marcoyip21 <122388164+marcoyip21@users.noreply.github.com>
2023-01-10 20:08:49 -0500

    first commit
    added data generato.R

--- dataGenerato.R
+++ dataGenerato.R
@@ -9,9 +9,10 @@ Limb_width= rnorm(100, mean = 4, sd =1)
 Limb_Length= rnorm(100, mean = 15, sd=5)
 
 #vector of 100 values for Observer, randomly selected from a vector of 3 names using sample function
-Observer= sample( c("Gee", "Glori", "Sally"), 100, replace=T)
+Observer= sample( c("Gee", "Bob", "Sally"), 100, replace=T)
 
 #Combine all of the vectors into a data.frame                  
 Data = data.frame(Species, Limb_width, Limb_Length, Observer)
 
 write.csv(Data,"/Users/marcoyip/Desktop/BIOL 432/measurements.csv" )
+
