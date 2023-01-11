8231f27c26c267663cd3049ef3ca0d303115d525
marcoyip21 <122388164+marcoyip21@users.noreply.github.com>
2023-01-11 15:29:05 -0500

    made edits
    made edits

--- volumeEstimato.R
+++ volumeEstimato.R
@@ -6,3 +6,6 @@ Data=read.csv("./measurements.csv")
 
 #Adding new column volume
 Data1=mutate(Data, volume = pi*(Limb_width/2)^2*Limb_Length)
+
+write.csv(Data1, "/Users/marcoyip/Desktop/BIOL 343/BIOL432_Assignment1/measurements.csv", row.names = F)
+
